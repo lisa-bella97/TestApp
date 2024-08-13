835d3f9587461729769fe942f0d9bda691b7a8c0
Elizaveta Prokhorova <Prohorova@rutoken.ru>
2024-08-13 16:40:47 +0300

    Add minimal sample

---
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.testapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.testapp.ui.theme.TestAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        Log.d("MY_LOGS", "Loading library jnidispatch")
+        System.loadLibrary("jnidispatch") // This causes crash on Android 15 emulator with 16 KB page size
+        Log.d("MY_LOGS", "jnidispatch is loaded")
+        setContent {
+            TestAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TestAppTheme {
+        Greeting("Android")
+    }
+}
